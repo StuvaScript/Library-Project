551402a32833f031fcb9263fce7e3e081a8abd76
Stu <stumac1000@yahoo.com>
2022-10-27 13:49:38 -0700

    Continue adding functionality

--- script.js
+++ script.js
@@ -5,5 +5,12 @@ function Book() {
 };
 
 function addBookToLibrary() {
-    
+
 };
+
+let formAppearBtn = document.querySelector('.form-appear-btn');
+let formPicker = document.querySelector('.form-wrap');
+
+formAppearBtn.addEventListener('click', () => {
+    formPicker.classList.remove('form-invisible');
+});
