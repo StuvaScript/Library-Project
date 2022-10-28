b9d61495dc859dfb8d20991eb9c6e1d90f727e42
Stu <stumac1000@yahoo.com>
2022-10-27 18:29:14 -0700

    Save often

--- script.js
+++ script.js
@@ -9,8 +9,24 @@ function addBookToLibrary() {
 };
 
 let formAppearBtn = document.querySelector('.form-appear-btn');
-let formPicker = document.querySelector('.form-wrap');
+let formVisible = document.querySelector('.form-wrap');
 
 formAppearBtn.addEventListener('click', () => {
-    formPicker.classList.remove('form-invisible');
+    formVisible.classList.remove('invisible');
 });
+
+let submitBtn = document.querySelector('#submit');
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    let title = document.querySelector('#title').value;
+    console.log(title);
+    let author = document.querySelector('#author').value;
+    console.log(author);
+    let radio = document.querySelectorAll('[type="radio"]')
+        .forEach(radio => {
+            if (radio.checked) {
+                console.log(radio.value);
+            }
+        });
+})
